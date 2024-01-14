d3b35861a49b53f6f24d5f14de5ba2d6d728d7fa
Robocotik <alexander.starkin@gmail.com>
2024-01-14 18:14:04 +0300

    [INIT] project

--- v17.cpp
+++ v17.cpp
@@ -3,12 +3,6 @@
 #include <windows.h>
 using namespace std;
 
-void showWord(char* s, int size){
-    for(int i=0; i < size; i++){
-        cout << s[i];
-    }
-}
-
 int main(int argc, char** argv){
     system("chcp 1251>nul");
     int n =  atoi(argv[1]); // это для второго задания в билете, тут оно не нужно, но полезно знать это
